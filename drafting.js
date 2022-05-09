01a149d1c10e54629b7f14418529428b7c91a46f
Casey Geist <tonewardbound@gmail.com>
2022-05-09 12:31:28 -0400

    tested drafting with "3 dumb bots"

--- drafting.js
+++ drafting.js
@@ -1,8 +1,4 @@
-
-//var fs = require('fs');
-
 // 113 rares, 95 uncommons, 75 commons, 15 basic lands
-
 const rares = [692, 829, 598, 830, 737, 646, 878, 831, 879, 739, 600, 834, 835, 695, 784, 604, 742, 698, 
             845, 608, 654, 656, 609, 610, 701, 791, 744, 848, 745, 747, 611, 797, 748, 749, 612, 799, 
             800, 614, 615, 617, 854, 620, 622, 624, 757, 666, 760, 761, 762, 707, 667, 708, 709, 807, 
@@ -27,8 +23,164 @@ const commons = [832, 694, 838, 839, 840, 841, 842, 743, 700, 651, 846, 847, 785
 
 const basics = [890, 889, 888, 896, 894, 895, 893, 892, 891, 897, 898, 899, 877, 876, 875];
 
-let pack1 = [];
-let random;         // random card from pools of R, U, C, B
+function createPack() {
+    let cardArray = [];      // the pack to be made
+    let random;         // random card from pools of R, U, C, B
+
+    // Get random slot #s for the 2 basic lands != 5 to prevent consecutive lands
+    const landPos = Math.floor(Math.random() * 5) ;    // Commons 0 - 4
+    const landPos2 = Math.floor(Math.random() * 5) + 6;   // Commons 6 - 10
+
+    // Push 9 Commons + 2 basic lands to pack
+    for( let i=0; i < 11; i++) {
+        // if this current index is a basic land slot, push a land there
+        if( i == landPos || i == landPos2 ) {
+            cardArray.push( basics[ Math.floor(Math.random() * 15) ] );
+            continue;
+        }
+                                                //@TODO: SPLIT THIS UP?
+        // protect against duplicate commons!
+        random = Math.floor(Math.random() * 75);
+        // if that common is already present, get another one
+        while( cardArray.includes( commons[random], 0) ) {
+            random = Math.floor(Math.random() * 75);
+        }
+        cardArray.push( commons[random] );
+    }
+
+    // Push 1 Rare to pack
+    random = Math.floor(Math.random() * 113);
+    cardArray.push( rares[random] );  
+
+    // Push 3 Uncommons to pack
+    for( let i=0; i < 3; i++) {
+        random = Math.floor(Math.random() * 95);
+        cardArray.push( uncommons[random] );
+    }
+
+    return cardArray;
+}
+
+//////////////////////////////////////////////////////////////////////////
+
+let pack1 = createPack();
+let pack2 = createPack();
+let pack3 = createPack();
+let pack4 = createPack();
+let indexOfLastCard = 14;
+let imgNum;
+let btnNext = document.getElementById("next-btn");
+let imageOfCard = document.getElementById("top-card");
+let cardsSeen = document.getElementById("cards-seen");
+let cardsKept = [];
+let passBtn = document.getElementById('pass-btn');
+let zIndex = 15;
+let cardIsChosen = false;
+
+
+
+// Show 1st card right away
+imgNum = pack1[indexOfLastCard--];
+imageOfCard.src = `images/${imgNum}.jpeg`;
+
+btnNext.addEventListener( 'click', e => {
+    let backing = document.createElement('div');
+    let seen = document.createElement('img');
+    seen.id = `${imgNum}`;
+
+    backing.classList.add('backing');
+    backing.style = `position: relative; z-index: ${zIndex--}`;
+
+    seen.classList.add('seen');
+    seen.src = `images/${imgNum}.jpeg`;
+    seen.style.opacity = "1";
+
+    backing.appendChild(seen);
+    cardsSeen.appendChild(backing);
+
+    seen.addEventListener( 'click', e => {  // allow selection of 1 card and
+        if(cardIsChosen == false) {
+            cardIsChosen = true;
+            backing.style.zIndex = 16;
+            seen.style.opacity = "0.5";
+            const chosenImgNum = Number( `${seen.id}`);
+            console.log(`keeping card # ${chosenImgNum}`);
+            cardsKept.push( chosenImgNum );
+            pack1.splice( pack1.indexOf( chosenImgNum, 0 ), 1 );
+        }
+    });
+    
+    imgNum = pack1[indexOfLastCard--];
+    imageOfCard.src = `images/${imgNum}.jpeg`;
+    if( indexOfLastCard == -1) {
+        imageOfCard.src = "";
+        displayCardBelow(imgNum, zIndex);
+        
+        // show button to pass the rest
+        // cardsSeen.style.pointer = "pointer";
+        btnNext.hidden = true;
+        passBtn.hidden = false;
+        passBtn.addEventListener( 'click', e => {
+            //passBtn.hidden = true;
+            // remove a card from pack2 and pack3
+            let botsPick = Math.floor(Math.random * pack2.length);
+            pack2.splice( botsPick, 1);
+            botsPick = Math.floor(Math.random * pack3.length);
+            pack3.splice( botsPick, 1);
+            botsPick = Math.floor(Math.random * pack4.length);
+            pack4.splice( botsPick, 1);
+            // rotate packs with 1 less card, e.g. clockwise
+            const temp = pack1;
+            pack1 = pack4;
+            pack4 = pack3;
+            pack3 = pack2;
+            pack2 = temp;
+            
+            cardsSeen.innerHTML = "";
+            cardIsChosen = false;
+            // display cards of pack received
+            for( let i = pack1.length-1; i > -1; i-- ) {
+                displayCardBelow( pack1[i], i );
+            }
+            if(pack1.length == 0) localStorage.setItem("extra-pack", cardsKept);
+        });
+    }
+});
+
+
+function displayCardBelow( iNum, currentZ ) {
+    let backing = document.createElement('div');
+    let seen = document.createElement('img');
+    
+    seen.id = `${iNum}`;
+
+    backing.classList.add('backing');
+    backing.style = `position: relative; z-index: ${currentZ--}`;
+
+    seen.classList.add('seen');
+    seen.src = `images/${iNum}.jpeg`;
+    seen.style.opacity = "1";
+
+    backing.appendChild(seen);
+    cardsSeen.appendChild(backing);
+
+    seen.addEventListener( 'click', e => {
+        if(cardIsChosen == false) {
+            cardIsChosen = true;
+            backing.style.zIndex = 16;
+            seen.style.opacity = "0.5";
+            const chosenImgNum = Number( `${seen.id}`);
+            console.log(`keeping card # ${chosenImgNum}`);
+            cardsKept.push( chosenImgNum );
+            pack1.splice( pack1.indexOf( chosenImgNum, 0 ), 1 );
+        }
+    });
+}
+
+
+
+
+
 
 // let cardsJSON = JSON.parse( fs.readFileSync('2ED.json', {encoding:'utf-8', flag:'r'}) );
 // let cardsInSet = cardsJSON["cards"];
@@ -43,107 +195,12 @@ let random;         // random card from pools of R, U, C, B
 //             rares.push(card);
 //     }
 // }
-let imgNum;
-let btnNext = document.getElementById("next-btn");
-let imageOfCard = document.getElementById("top-card");
-let cardsSeen = document.getElementById("cards-seen");
-let passBtn = document.getElementById('pass-btn');
-let zIndex = 15;
-let cardIsSelected = false;
-
-// Get random slot #s for the 2 basic lands != 5 to prevent consecutive lands
-let landPos = Math.floor(Math.random() * 5) ;    // Commons 0 - 4
-let landPos2 = Math.floor(Math.random() * 5) + 6;   // Commons 6 - 10
-
-// Push 9 Commons + 2 basic lands to pack
-for( let i=0; i < 11; i++) {
-    // if this current index is a basic land slot, push a land there
-    if( i == landPos || i == landPos2 ) {
-        pack1.push( basics[ Math.floor(Math.random() * 15) ] );
-        continue;
-    }
-    
-    // protect against duplicate commons!
-    random = Math.floor(Math.random() * 75);
-    // if that common is already present, get another one
-    while( pack1.includes( commons[random], 0) ) {
-        random = Math.floor(Math.random() * 75);
-    }
-    pack1.push( commons[random] );
-}
-
-// Push 1 Rare to pack
-random = Math.floor(Math.random() * 113);
-pack1.push( rares[random] );  
-
-// Push 3 Uncommons to pack
-for( let i=0; i < 3; i++) {
-    random = Math.floor(Math.random() * 95);
-    pack1.push( uncommons[random] );
-}
-
-
-// Show 1st card right away
-imgNum = pack1.pop();
-imageOfCard.src = `images/${imgNum}.jpeg`;
-
-btnNext.addEventListener( 'click', e => {
-    let backing = document.createElement('div');
-    let seen = document.createElement('img');
-
-    backing.classList.add('backing');
-    backing.style = `position: relative; z-index: ${zIndex--}`;
-
-    seen.classList.add('seen');
-    seen.src = `images/${imgNum}.jpeg`;
-    seen.style.opacity = "1";
-
-    backing.appendChild(seen);
-    cardsSeen.appendChild(backing);
-
-    seen.addEventListener( 'click', e => {
-        if(cardIsSelected == false) {
-            cardIsSelected = true;
-            backing.style.zIndex = 16;
-            seen.style.opacity = "0.3";
-            
-        }
-        // else {
-        //     cardIsSelected = false;
-        //     seen.style.opacity = "1";
-        // }
-    });
-    
-    imgNum = pack1.pop();
-    imageOfCard.src = `images/${imgNum}.jpeg`;
-    if( pack1.length == 0) {
-        btnNext.disabled = true;
-        btnNext.innerText = ":-)";
-        // allow selection of 1 card and
-        // show button to pass the rest
-        cardsSeen.style.pointer = "pointer";
-        btnNext.hidden = true;
-        passBtn.hidden = false;
-    }
-});
-
-
-
-
-
-
-
-
-//fs.writeFileSync('commons.txt', )
-
-
-
-
 
 
 
 
 // WORKED!  BUT ONLY NEEDED ONCE!  DON'T USE AGAIN!
+// var fs = require('fs');
 // const https = require("https");
 
 // for( let id of idNums ) {
@@ -153,3 +210,6 @@ btnNext.addEventListener( 'click', e => {
 //         response.pipe( file );
 //     });
 // }
+
+
+
