69b36bd68fa6b07d4751962605763e3e162971ba
Casey Geist <tonewardbound@gmail.com>
2022-06-03 17:19:03 -0400

    added a bunch more events for server to handle

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-import Tournament from './classes.js';
-import createPack from './createpack.js';
-
-//const { buildDeck } = require('./build');
+import Tournament from './mclasses.mjs';
+import createPack from './createpack.mjs';
 
 // Global constants
 const PORT = 29170;   // or port # 13207 for MTG  :)
+const PLAYERS_PER_DRAFT = 2;
 
 // Global variables
 let cards;
@@ -15,55 +14,35 @@ let playersReady = 0;
 let activeDrafts = [];      // @TODO: have a way to halt joining after current sign-up for server maint.
 
 // server-app modules
-const express = require('express');
+import express from 'express';
 const app = express();
-const http = require('http');
+import http from 'http';
 const server = http.createServer(app);
-const { Server } = require('socket.io');
+import { Server } from 'socket.io';
 let io = new Server(server);       // An Express HTTP server listening on port PORT ready for socket connections
-const fs = require('fs');
+import fs from 'fs';
 
+// Not needed just yet
+// try {
+//     cards = JSON.parse( fs.readFileSync('2ED.json', 'utf-8', (err, data) => {
+//         if(err) throw err;
+//         console.log("data read");
+//     }) )["cards"];  
 
-try {
-    cards = JSON.parse( fs.readFileSync('2ED.json', 'utf-8', (err, data) => {
-        if(err) throw err;
-        console.log("data read");
-    }) )["cards"];  
-
-} catch (err) {
-    console.log(err);
-}
-
+// } catch (err) {
+//     console.log(err);
+// }
 
 app.use( express.static('./public') );
-
-// app.use(express.cookieParser());
-// app.use( express.cookieSession({ key: 'value', cookie: {maxAge: 13200000} }) );
-// cookie middleware
-
-// app.use( express.urlencoded({ extended: false }) );
-
-// @TODO How to add specific file paths to resources with app.get or app.use ???
-// ?  __dirname + '../node_modules/jquery/dist/jquery.js'  ?
-
-// app.get('/', (req, resp) => {
-//     resp.status(200).sendFile(__dirname + '/index.html');
-// });
 
 // app.get('/cards', (req, resp) => {
 //     console.log("path is: " + req.path);
 //     resp.json( cards );                      // WORKED because resource was pre-loaded here
 // });
 
-// app.post('/test.php', (req, resp) => {
-//     // resp.json(`{\"userName\":\"${req.body.userName}\", \"computedString\":\"Hello, ${req.body.userName}\"}`);
-//     resp.json( { userName: req.body.userName, computedString: `Hello, ${req.body.userName}, good day!`} );
-// });
-
 app.all('*', (req, resp) => {
     resp.status(404).send('resource not found');
 });
-
 
 server.listen(PORT, () => console.log(`Express HTTP server listening on port ${PORT}`) );
 
@@ -72,123 +51,262 @@ io.on('connection', (socket) => {
     
     console.log("User connected and given ID " + socket.id);
     currentNumUsers++;
-    io.emit('userCount', {numUsers: currentNumUsers} );
-    socket.emit('listData', {players: playersJoined} );
+    io.emit('userCount', {numUsers: currentNumUsers} );     // Give new user count to EVERYONE
+    socket.emit('listData', {players: playersJoined} );     // Give the current list to user/client (socket) just joining
 
+    // Client has requested to join the next draft
     socket.on('playerJoinRequest', (data) => {
-        if( playersJoined.length < 16) {
-            playersJoined.push( socket.id );
-            playersJoined.push( data.name );
-            socket.join(`draft${draftNumber}`);
-            socket.emit('playerJoinOK', {draftNum: draftNumber, playerNum: (playersJoined.length / 2) } );      
-            socket.broadcast.emit('newPlayerJoined', {players: playersJoined} )
-        }
-        //console.log("\"playerJoined\" event captured. Player accepted into draft group");
-        else if( playersJoined.length > 14) {
-            // broadcast ''newDraftSignup' event for clients in current draft group to clear list
+        // First make sure username is unique, at least within the draft group
+        if( playersJoined.includes(data.name) ) {
+            return;                             // @TODO: send back join failed event/message and popup tip
+        }            
+        // sign up the requesting client
+        playersJoined.push( socket.id );        // unique 20-digit ID
+        playersJoined.push( data.name );        // name entered by user
+        socket.join(`draft${draftNumber}`);     // group (or "room") ID
+
+        socket.emit('playerJoinOK', {draftNum: draftNumber, playerNum: (playersJoined.length / 2 - 1) } );      
+        socket.broadcast.emit('newPlayerJoined', {players: playersJoined} );
+ 
+        console.log(`${playersJoined.length / 2} players have joined`)
+        
+        // if we DO have 8 players signed up...
+        if( playersJoined.length == 2 * PLAYERS_PER_DRAFT) {    
+            // broadcast notification for clients in current draft group to clear list
             io.to(`draft${draftNumber}`).emit('clearSignup');
 
-            // start draft with the 8 players in draft N, using data in playersJoined array
+            // start draft with the P players in draft N, using data in playersJoined array...
             let names = [];
-            for(let i = 1; i < 16; i=i+2) names.push( playersJoined[i] );
+            for(let i = 1; i < (2*PLAYERS_PER_DRAFT); i=i+2) names.push( playersJoined[i] );
 
-            // and new Tourament object
+            // ...and a new Tourament object
             activeDrafts.push( new Tournament(draftNumber, names) );  // Tournament includes iTimer pre-set to 30:00
-                // broadcast to players in draft group N
-                io.to(`draft${draftNumber}`).emit('prepareForDraft' );
-
-                // they request a pack
-                // their JS redirects to open pack / start draft screen
-                
-                // remember to switch direction of rotation for 2nd pack
             
+            // notify players in draft group N to prepare to draft the first pack
+            io.to(`draft${draftNumber}`).emit('prepareForDraft' ); 
         }
     });
 
-    // server sends player a pack
+    //////////////////// DRAFT EVENTS (no pun intended) ////////////////////
+
+    // Client has requested the 1st pack to open/draft
     socket.on('packRequest', () => {
-        socket.emit('firstPack', { pack: createPack() } );
+        socket.emit('firstPack', { pack: createPack() } );  // server sends player a pack
     });
 
+    // Client is now ready to begin drafting
     socket.on('readyToDraft', () => {
-        if(playersReady == 8) {
+        playersReady++;
+        // all 8 players ready?
+        if(playersReady === PLAYERS_PER_DRAFT) {
             let thisDraft = activeDrafts[draftNumber]
             // notify players to start their timers
             io.to(`draft${draftNumber}`).emit('startDraftTimer');
 
-            // start draft timer 
-            thisDraft.startDraft();  // starts the timer
+            // thisDraft.setupDisplay( (m,s) => console.log(m + ":" + s) );   // No longer needed with separate "server-timer.mjs" file
 
-            // necessary to send time update every 10 seconds ?
+            // @TODO: Send time update every 30 seconds ?
             // setInterval( () => {
-            //     io.to(`draft${draftNumber}`).emit('timeRemaining', activeDrafts[draftNumber].getCurrentTime() );
-            // }, 10000 );
+            //     io.to(`draft${draftNumber}`).emit('timeRemaining', thisDraft.getCurrentTime() );
+            // }, 30000 );
 
-            // reinitialize playersJoined array
+            // reinitialize playersJoined array for the next draft group
             playersJoined.length = 0;
 
             // reset players ready
             playersReady = 0;
 
-            io.to(`draft${draftNumber}`).emit('beginDrafting', {packNum: 1} );
+            thisDraft.startDraft();     // start server's draft timer 
+
             // inc draft #
-            draftNumber++;
+            draftNumber++;   // @TODO: IMPORTANT - move this up earlier in case players are wanting to join the next draft
         }
-        else playersReady++;
     });
 
-    socket.on('cardPicked', (data) => {     // draft ID, player ID, cards being passed
+    socket.on('timerStarted', () => {
+        socket.emit('beginDrafting', {packNum: 1} );   
+        // @TODO:  Switch to notifying all to begin:  io.to(`draft${draftNumber}`).emit('beginDrafting'} );
+    });
+
+    // Client has picked a card from their cards and can rotate the rest,
+    // or request another pack if no cards are left,
+    // or begin assembling their deck if no packs are left to draft
+    socket.on('cardPicked', (data) => {     // { draftNum, playerNum, theCards }
+
         let thisDraft = activeDrafts[data.draftNum];
-
-        if( data.theCards.length == 0 ) {   // if no cards left in pack to rotate
-            // inc pack # (move on to the next pack)
-            thisDraft.currentPack++;
-            if( thisDraft.currentPack > 3) {
-                // we are done drafting! time to build a deck!  =D
-                io.to(`draft${draftNumber}`).emit('assembleDeck');
-            }
-            else {
-                // prepare to issue a new pack to each player
-                io.to(`draft${draftNumber}`).emit('nextPackReady');                
-            }
-        }  
-        else {
-            thisDraft.theirPacks[data.playerNum] = data.theCards;
-            thisDraft.incNumPlayersReady();
-            if( thisDraft.getNumPlayersReady() === 8 ) {
+        console.log("received cardPicked event from player " + data.playerNum + " in draft " + data.draftNum +
+                " passing " + data.theCards.length + " cards");
                 
-                // rotate cards/packs L or R depending on pack#
-                thisDraft.rotatePacks();
-                // and give each player their rotated cards
-                io.to(`draft${draftNumber}`).emit('rotatedCardsReady');
+        thisDraft.setPlayersPack(data.playerNum, data.theCards);
+        thisDraft.incNumPlayersReady();
+        console.log(thisDraft.getNumPlayersReady() + " players ready to pass thier cards");
+        if( thisDraft.getNumPlayersReady() === PLAYERS_PER_DRAFT ) {
 
-                thisDraft.resetNumPlayersReady();
-            }
+                console.log("rotating cards");
+            // rotate cards/packs L or R depending on pack#
+            thisDraft.rotatePacks();
+            
+            // and notify each player their rotated cards are ready
+            io.to(`draft${data.draftNum}`).emit('rotatedCardsReady');
+            thisDraft.resetNumPlayersReady();
+
+            // After rotating, if just 1 card had been rotated (last card rotated in is kept)
+            if( data.theCards.length == 1 ) {   
+                
+                console.log("players just rotated and got back 1 card which is kept")
+    
+                // so inc pack # (move on to the next pack)
+                thisDraft.incPackNumber();
+                if( thisDraft.getPackNumber() > 3) {
+                    // we are done drafting! time to build a deck!  =D
+                    console.log("Finished with last pack. Time to build!" );
+                } 
+            }  
         }
+        
     });
 
-
+    // Client relies on the server with the current setup (subject to change) to
+    // get their cards passed from the adjacent player
     socket.on('rotatedCardsRequested', (data) => {  // data:  { draftNum, playerNum}
         let thisDraft = activeDrafts[data.draftNum];
-        socket.emit('rotatedCards', { cards: thisDraft.theirPacks[data.playerNum] } );
+        socket.emit('rotatedCards', { cards: thisDraft.getPlayersPack(data.playerNum) } );
     });
 
-    socket.on('nextPackRequested', (data) => {
-        socket.emit('nextPack', { pack: createPack(), packNum: activeDrafts[data.draftNum].currentPack } );
+    // Client has requested the next pack to open and draft
+    socket.on('nextPackRequested', (data) => {      // { draftNum, playerNum }
+        socket.emit('nextPack', { pack: createPack(), packNum: activeDrafts[data.draftNum].getPackNumber() } );
     }); 
 
-    socket.on('deckAssembled', (data) => {
+    //////////////////// BUILD EVENTS ////////////////////
 
+    // Client has assembled their deck and can get pairing info
+    socket.on('deckAssembled', (data) => {      // { draftNum, playerNum, theDeck }
+        let thisDraft = activeDrafts[data.draftNum];
+        // save their decks?
+        console.log("received deckAssembled event from player " + data.playerNum + " in draft " + data.draftNum );     
+        //thisDraft.setPlayersDeck(data.playerNum, data.theDeck);
+
+        thisDraft.incNumPlayersReady();
+        console.log(thisDraft.getNumPlayersReady() + " players ready to start playing");
+
+        // then if allAssembled, or numReady == 8
+        if( thisDraft.getNumPlayersReady() === PLAYERS_PER_DRAFT ) {
+
+            thisDraft.stopDraft();      // reset timers to 50:00
+            io.to(`draft${data.draftNum}`).emit('resetTimer');
+
+            thisDraft.calcPairings();   // calculate pairings
+
+            thisDraft.resetNumPlayersReady();
+
+            // tell all players to request pairing info
+            io.to(`draft${data.draftNum}`).emit('pairingInfoReady');
+        }
+    });
+
+    //////////////////// TOURNAMENT EVENTS (no pun intended) ////////////////////
+
+    // Client has requested their pairing info for Round 1, or for the first/next round
+    socket.on('pairingRequest', (data) => {    // { draftNum, playerNum }
+        // look up pairing info
+        const thisDraft = activeDrafts[data.draftNum];
+        const oppNum = thisDraft.getPairingInfo()[data.playerNum];
+        const oppName = thisDraft.getPlayerName(oppNum);
+
+        let matchID;   
+        if( data.playerNum > oppNum ) matchID = `${oppNum}v${data.playerNum}`;   // e.g. 5v7 not 7v5
+        else matchID = `${data.playerNum}v${oppNum}`;                           // could use ? operator here
+        
+        // create match "room", e.g. draft0-match5v7
+        socket.join(`draft${data.draftNum}-match${matchID}`);         // IS THIS CORRECT?   
+
+        // @TODO: then UN-join for next round after match is over?
+
+        // send back pairing info      e.g. 5v7,                   5 (or 7),            lizardman
+        socket.emit('pairingInfo', { match: matchID, opponentNum:  oppNum, opponentName: oppName } );
+    })
+
+    // Client has recevied their pairing info and is ready to begin the tournament
+    socket.on('readyToPlay', (data) => {              // { draftNum, playerNum }
+        let thisDraft = activeDrafts[data.draftNum];
+
+        // another player ready
+        thisDraft.incNumPlayersReady();
+        if(thisDraft.getNumPlayersReady() == PLAYERS_PER_DRAFT) {
+            io.to(`draft${data.draftNum}`).emit('beginMatch');
+            
+            // start round
+            thisDraft.startRound();
+
+            // they begin playing
+
+            // reset players ready?
+            thisDraft.resetNumPlayersReady();
+        }
+    })
+
+    socket.on("privateMsg", (data) => {
+        console.log("private message received: " + data.msg);
+        console.log("passing along to your opponent in match " + data.ourMatchID);
+        //socket.to(`${data.theID}`).emit("privateMsg", { theID: socket.id, msg: data.msg} );
+        // socket.to("match0v1").emit("privateMsg", {msg: data.msg} );
+        socket.to(`draft${data.ourDraftID}-match${data.ourMatchID}`).emit("privateMsg", {msg: data.msg} );
+    });   // e.g. "draft2-match5v7"
+
+    
+
+    socket.on("nextPhase", (data) => {    // ourDraftID, ourMatchID       ...phase, e.g. "draw" or "main1" or "combat"
+        socket.to(`draft${data.ourDraftID}-match${data.ourMatchID}`).emit("nextPhase");
+    });
+
+    socket.on("handRevealed", (data) => {
+        socket.to(`draft${data.ourDraftID}-match${data.ourMatchID}`).emit("handRevealed", {theirHand: data.hand} );
+    });
+
+    socket.on("resigns", (data) => {    // draftID, playerID, oppID
+        // I resign, my opponent wins this game
+
+        let thisDraft = activeDrafts[data.draftID];
+
+        // Record the game result for both players, perhaps match result too if 0-2
+        let result = thisDraft.players[oppID].addGameResult( 1 );
+        thisDraft.players[playerID].addGameResult( 0 );
+
+        socket.emit("youResigned");
+        
+        if(result == 0)  // play another 
+            socket.to(`draft${data.ourDraftID}-match${data.ourMatchID}`).emit("nextGame");
+        else 
+            socket.to(`draft${data.ourDraftID}-match${data.ourMatchID}`).emit("matchDone");
+    });
+
+    socket.on("draw", (data) => {                       // @TODO:  DRY this out!
+        let thisDraft = activeDrafts[data.draftID];
+
+        // Record the game result for both players
+        let result = thisDraft.players[oppID].addGameResult( 0.5 );
+        thisDraft.players[playerID].addGameResult( 0.5 );
+
+        if(result == 0)
+            socket.to(`draft${data.ourDraftID}-match${data.ourMatchID}`).emit("nextGame");
+        else 
+            socket.to(`draft${data.ourDraftID}-match${data.ourMatchID}`).emit("matchDone"); 
+        
+    })
+
+    socket.on("leavesMatch", (data) => {
+        socket.leave(`draft${data.ourDraftID}-match${data.ourMatchID}`);
     });
 
 
 
-
+    //////////////////// OTHER EVENTS ////////////////////
 
     // when user requests info/image for a card
     socket.on('cardSearchRequest', (data) => {
         let myCard;
-        let cardInfo;
+        let cardInfo;                               // @TODO:  FIX "UNDEFINED" COST for LANDS
         let cardData = {};
         
         myCard = cards.find( card => String(card.name).toLowerCase() == String(data.cardName).toLowerCase() );
@@ -214,46 +332,22 @@ io.on('connection', (socket) => {
     socket.on('disconnecting', () => {
         console.log(`User with ID ${socket.id} disconnecting`);
         currentNumUsers--;
-        // playersJoined.splice( playersJoined.indexOf( socket.id ), 2);
-        socket.broadcast.emit('userCount', {numUsers: currentNumUsers} );
+        // playersJoined.splice( playersJoined.indexOf( socket.id ), 2);    // Probably not necessary unless user changes mind right away
+        socket.broadcast.emit('userCount', {numUsers: currentNumUsers} );  // Give updated count to everyone ELSE
         // socket.broadcast.emit('listData', {players: playersJoined} );
     });
 });
 
 
-/* 
+// Notes:
+//  const userId = await fetchUserId(socket);       userId the same for all devices/tabs ??
+//  socket.join(userId);
 
-// server-side code specific to the game can be stored in, e.g., gameserver.js
+//  // and then later
+//  io.to(userId).emit("hi");
 
+// io.to("match0v4").to("match1v3").to("match2v6").emit('timeUpdate');  // for notifying multiple matches but not all
+// socket.to("match5v7").emit('eventForPlayersOpponent');    // notifies all in room EXCEPT the sender (this socket)
+// socket.leave("match5v7");
 
-
-io.sockets.on('connection', (theSocket) => {
-    console.log("Client connected");
-    draft2ed.initGame(io, theSocket); // initGame sets up the event listeners for Socket.IO to allow browser & server to communicate in real time
-});                                                          // e.g. gameSocket.on('joinDraft', handleJoinDraft);
-*/
-
-
-
-
-
-/* 
-        game.addPlayer( {id: player.id, name: player.name, life: INIT_LIFE_POINTS} );
-*/
-
-
-/* CLIENT-SIDE:
-
-const INTERVAL = 400;
-
-const update = () => {
-    if(this.localPlayer != undefined) {
-        this.sendData();
-        this.localPlayer.doSomething();
-    }
-}
-
-setInterval( update, INTERVAL);
-
-
-*/
+// socket.rooms will/should contain all rooms joined 
